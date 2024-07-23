17fe32aceba8746b8379f997ee55c4f97b4b394a
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2024-07-24 02:20:19 +0900

    7월 24일 스플링 서블릿 공부

---
+++ servlet/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plane"); //헤더 정보에 들어감
+        response.setCharacterEncoding("utf-8"); //헤더 정보에 들어감
+        response.getWriter().write("hello " + username); //HTTP 메세지 바디에 데이터가 들어감
+
+
+
+    }
+}
