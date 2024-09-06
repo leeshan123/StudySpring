a7a0aa8908a0a3e411cb7087a701d6b9f67cd1b8
leeshan123 <84975698+leeshan123@users.noreply.github.com>
2024-09-07 04:52:09 +0900

    9월 7일 스프링 jpa JDBC공부

---
+++ jdbc/jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connection;
+//abstract로 객체 생성을 막아둠
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
